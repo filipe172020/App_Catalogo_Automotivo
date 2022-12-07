d3843db956c2c94e21c8126e4c11bbb146362b16
Filipe <filipemarquespascoal@hotmail.com>
2022-12-06 21:17:34 -0300

    routes

---
+++ Pages/Routes/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomSheetNavigator } from '@th3rdwave/react-navigation-bottom-sheet';
+import { View } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+const BottomSheet = createBottomSheetNavigator();
+
+export default function Routes() {
+
+}
+
+return (
+
+<View>Teste</View>
+
+);
